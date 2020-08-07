a223ac9d2829f3d4a35b0ee574bdb8b68cebc2df
paul1680 <67039598+paul1680@users.noreply.github.com>
2020-08-07 16:35:22 +0600

    new

---
+++ const.js
@@ -0,0 +1,17 @@
+const d = n => n % 7;
+const add = (x, y) => x + y;
+const g6 = () => 6;
+const fo = (x, y) => {
+     const s = x + y;
+     const di = x - y;
+     const t = s * di;
+     return t;
+}  
+const r = d(18);
+const a = add(3, 7);
+const g = g6();
+const v = fo(7, 2);
+console.log(a);
+console.log(r);
+console.log(g);
+console.log(v);
